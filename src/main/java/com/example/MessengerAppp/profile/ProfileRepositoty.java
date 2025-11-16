6a4894ab41235bc7d185e8d2b11c3bddfa818cb5
mohamedSennia <mohameddsennia@gmail.com>
2025-11-16 17:32:12 +0100

    fixed query

--- src/main/java/com/example/MessengerAppp/profile/ProfileRepositoty.java
+++ src/main/java/com/example/MessengerAppp/profile/ProfileRepositoty.java
@@ -3,12 +3,18 @@ package com.example.MessengerAppp.profile;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 import java.util.Optional;
 
 public interface ProfileRepositoty  extends JpaRepository<Profile,Integer> {
 Optional<Profile> findByOwnerEmail(String email);
-@Query("Select p FROM Profile p Where LOWER(p.profileName) LIKE LOWER(CONCAT('%', :keyWord, '%') or LOWER(p.owner.email) Like LOWER(CONCAT('%', :keyWord, '%')")
-List<Profile> searchProfile(String keyWord);
+    @Query("""
+    SELECT p
+    FROM Profile p
+    WHERE LOWER(p.profileName) LIKE LOWER(CONCAT('%', :keyWord, '%'))
+       OR LOWER(p.owner.email) LIKE LOWER(CONCAT('%', :keyWord, '%'))
+""")
+    List<Profile> searchProfiles(@Param("keyWord") String keyWord);
 }
