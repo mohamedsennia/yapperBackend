6da66217610969dc5e5c6463e0442d6c8d1c7ac4
mohamedSennia <mohameddsennia@gmail.com>
2025-11-16 18:20:28 +0100

    rollling back for testing

--- src/main/java/com/example/MessengerAppp/profile/ProfileRepositoty.java
+++ src/main/java/com/example/MessengerAppp/profile/ProfileRepositoty.java
@@ -3,18 +3,12 @@ package com.example.MessengerAppp.profile;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 import java.util.Optional;
 
 public interface ProfileRepositoty  extends JpaRepository<Profile,Integer> {
 Optional<Profile> findByOwnerEmail(String email);
-    @Query("""
-    SELECT p
-    FROM Profile p
-    WHERE LOWER(p.profileName) LIKE LOWER(CONCAT('%', :keyWord, '%'))
-       OR LOWER(p.owner.email) LIKE LOWER(CONCAT('%', :keyWord, '%'))
-""")
-    List<Profile> searchProfiles(@Param("keyWord") String keyWord);
+@Query("Select p FROM Profile p Where p.profileName LIKE %:keyWord% or p.owner.email Like %:keyWord%")
+List<Profile> searchProfile(String keyWord);
 }
